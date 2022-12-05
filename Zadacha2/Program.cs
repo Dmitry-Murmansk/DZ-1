add5b810981d027a72b8e2eddf5386432febbf8e
Dmitry-Murmansk <119297978+Dmitry-Murmansk@users.noreply.github.com>
2022-12-05 12:02:39 +0300

    Нахождение макс из трех

---
+++ Zadacha2/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("");
+int c = Convert.ToInt32(Console.ReadLine());
+
+int max = a;
+
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+
+Console.Write("max ");
+Console.WriteLine(max);
