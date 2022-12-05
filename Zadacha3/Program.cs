e45e03354b06563c9d91f591077da1e66f81415e
Dmitry-Murmansk <119297978+Dmitry-Murmansk@users.noreply.github.com>
2022-12-05 13:15:02 +0300

    Веление числа на четное и нечетное

---
+++ Zadacha3/Program.cs
@@ -0,0 +1,13 @@
+﻿int a;
+Console.WriteLine("Введите число: ");
+a = Convert.ToInt32(Console.ReadLine());
+
+if(a % 2 == 0 )
+{
+    Console.WriteLine("чётное число");
+}
+else
+{
+    Console.WriteLine("нечётное");
+}
+Console.ReadKey();
